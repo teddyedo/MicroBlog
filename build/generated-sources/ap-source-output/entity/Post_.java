13648e69aaf17dc788e152d0e0ea604d5d171647
Edoardo <35118807+teddyedo@users.noreply.github.com>
2020-02-21 11:20:37 +0100

    fixed problems

--- build/generated-sources/ap-source-output/entity/Post_.java
+++ build/generated-sources/ap-source-output/entity/Post_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-02-13T20:48:29")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-02-14T11:35:26")
 @StaticMetamodel(Post.class)
 public class Post_ { 
 
--- build/generated-sources/ap-source-output/entity/Utente_.java
+++ build/generated-sources/ap-source-output/entity/Utente_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-02-13T20:48:29")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-02-14T11:35:26")
 @StaticMetamodel(Utente.class)
 public class Utente_ { 
 
